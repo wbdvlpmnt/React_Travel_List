5d665c934781344f69c1cca5e81b0f478f40bac6
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-17 16:01:14 -0600

    render list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -20,7 +25,19 @@ function Form() {
   );
 }
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => {
+          return <Item item={item} />;
+        })}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return <li>{item.description}</li>;
 }
 function Stats() {
   return (
