ddbfd215535e72b4cf92db663a5b8aa396eb3972
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-17 16:27:38 -0600

    adding state to select form element

--- src/App.js
+++ src/App.js
@@ -22,13 +22,14 @@ function Logo() {
 }
 function Form() {
   const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
   function handleSubmit(e) {
     e.preventDefault();
   }
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3> What do you need for your trip 😍</h3>
-      <select>
+      <select value={quantity} onChange={(e) => setQuantity(e.target.value)}>
         {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
