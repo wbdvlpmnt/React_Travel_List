d775e75bfa67f7c3e5105dccbfc02d2048aa84d1
wbdvlpmnt <jai.gokhale5219@gmail.com>
2023-11-17 15:56:23 -0600

    scaffold the app

--- src/App.js
+++ src/App.js
@@ -1,5 +1,31 @@
-function App() {
-  return <div>Hello React</div>;
+export default function App() {
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
 
-export default App;
+function Logo() {
+  return <h1>🏝️ Far Away 💼</h1>;
+}
+function Form() {
+  return (
+    <div className="add-form">
+      <h3> What do you need for your trip 😍</h3>
+    </div>
+  );
+}
+function PackingList() {
+  return <div className="list">List</div>;
+}
+function Stats() {
+  return (
+    <footer className="stats">
+      💼 You have packed X items on your list, and you have already packed X%
+    </footer>
+  );
+}
